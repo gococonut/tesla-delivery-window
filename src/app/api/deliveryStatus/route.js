33d9716bdf9c0dd8edf07c4dc55d5b8fb32c80f6
gococonut <gococonu7@outlook.com>
2024-10-25 13:16:33 +0800

    delivery window

---
+++ src/app/api/deliveryStatus/route.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+export async function POST(req) {
+  const body = await req.json(); // 解析请求体
+  const { orderNum, token } = body;
+
+  const url = "https://akamai-apigateway-vfx.tesla.cn/tasks";
+  const params = {
+    deviceLanguage: "zh",
+    deviceCountry: "CN",
+    referenceNumber: orderNum,
+    appVersion: "4.37.5-2886",
+  };
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    Host: "akamai-apigateway-vfx.tesla.cn",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-Hans",
+    "Cache-Control": "no-cache",
+    charset: "utf-8",
+    "User-Agent": "Tesla/2886 CFNetwork/1568.100.1.2.1 Darwin/24.0.0",
+    Connection: "keep-alive",
+    "x-tesla-user-agent": "TeslaApp/4.37.5/b81662c4ab/ios/18.0.1",
+    Accept: "*/*",
+  };
+
+  try {
+    const response = await axios.get(url, { params, headers });
+    const data = response.data;
+    const messageTitle = data?.tasks?.scheduling?.card?.messageTitle;
+    const messageBody = data?.tasks?.scheduling?.card?.messageBody;
+    const estimatedDeliveryDates = `${messageTitle}${messageBody}`;
+
+    if (messageTitle == undefined) {
+      return NextResponse.json(
+        { message: "token 已失效，请重新生成" },
+        { status: 200 }
+      );
+    }
+
+    if (estimatedDeliveryDates) {
+      return NextResponse.json(
+        { message: estimatedDeliveryDates },
+        { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: `预计交付时间：${data?.tasks?.scheduling?.deliveryWindowDisplay}`,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: `无效的订单号或 token`,
+      },
+      { status: 400 }
+    );
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,131 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ToastAction } from "@/components/ui/toast";
+const FormSchema = z.object({
+  orderNum: z.string().min(1, {
+    message: "required",
+  }),
+  token: z.string().min(1, {
+    message: "required",
+  }),
+});
 
-export default function Home() {
+function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      orderNum: "",
+      token: "",
+    },
+  });
+
+  async function checkDeliveryStatus(orderNum, token) {
+    const response = await axios.post("/api/deliveryStatus", {
+      orderNum,
+      token,
+    });
+    return response.data.message;
+  }
+
+  async function onSubmit(data) {
+    setIsLoading(true);
+    try {
+      const message = await checkDeliveryStatus(data.orderNum, data.token);
+      setIsLoading(false);
+      toast({
+        title: "Delivery Window",
+        description: message,
+        action: <ToastAction altText="确定">OK</ToastAction>,
+        duration: 120000,
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast({
+        variant: "destructive",
+        title: "invalid order number or token",
+      });
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div
+      className="flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/background.jpg')" }}
+    >
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Tesla Delivery Window</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="orderNum"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Order Number</FormLabel>
+                    <FormControl>
+                      <Input placeholder="RN809000000" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+              <FormField
+                control={form.control}
+                name="token"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Access Token</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      iPhone 下载 Auth for Tesla 获取，安卓自行搜索相关 app。
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={isLoading}>
+                {isLoading ? "..." : "Submit"}
+              </Button>
+            </form>
+          </Form>{" "}
+        </CardContent>
+      </Card>
     </div>
   );
 }
+
+export default Home;
