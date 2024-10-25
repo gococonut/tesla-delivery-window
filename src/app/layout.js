b3b5ed3d40d03e73a97eeb828bbcd3d9887b2340
gococonut <gococonu7@outlook.com>
2024-10-25 17:33:47 +0800

    page analytics display

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
+import { FaEye, FaUsers } from "react-icons/fa"; // Importing icons from react-icons
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -32,6 +32,18 @@ export default function RootLayout({ children }) {
           data-page-uv-id="page_uv"
         ></script>
         {children}
+        <div className="fixed bottom-0 left-0 w-full bg-black bg-opacity-70 text-white text-center py-4 z-50 flex justify-center items-center space-x-4">
+          <div className="flex items-center space-x-2 text-[0.7rem]">
+            <FaEye />
+            <span>本页访问人次:</span>
+            <span id="page_pv" className="text-[0.7rem]"></span>
+          </div>
+          <div className="flex items-center space-x-2 text-[0.7rem]">
+            <FaUsers />
+            <span>本页访问人数:</span>
+            <span id="page_uv" className="text-[0.7rem]"></span>
+          </div>
+        </div>
         <Toaster />
       </body>
     </html>
